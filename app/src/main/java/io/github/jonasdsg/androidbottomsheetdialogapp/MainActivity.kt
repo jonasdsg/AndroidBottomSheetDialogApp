43a235c3f00324e8f956b1064b798e19df51fb81
Jonas Góes <jonasdsg1@gmail.com>
2024-01-09 21:34:15 -0300

    - Created the modal view - set viewBind as true in build.gradle - Injecting the view with bind in MainActivity - showing modal on click in show button - adjusts

--- app/src/main/java/io/github/jonasdsg/androidbottomsheetdialogapp/MainActivity.kt
+++ app/src/main/java/io/github/jonasdsg/androidbottomsheetdialogapp/MainActivity.kt
@@ -2,10 +2,29 @@ package io.github.jonasdsg.androidbottomsheetdialogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import io.github.jonasdsg.androidbottomsheetdialogapp.databinding.ActivityMainBinding
+import io.github.jonasdsg.androidbottomsheetdialogapp.databinding.BottomSheetModalBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
+        activityMainBinding.button.setOnClickListener{
+            show()
+        }
+    }
+
+    private fun show(){
+        val dialog = BottomSheetDialog(this)
+        val bottomSheet = BottomSheetModalBinding.inflate(layoutInflater,null, false)
+        dialog.setContentView(bottomSheet.root)
+        dialog.show()
+        bottomSheet.button.setOnClickListener {
+            dialog.dismiss()
+        }
     }
 }
